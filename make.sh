6ab3417a6527cc171a7982a3e213b9e7114934b9
Christopher Brown <io@henrian.com>
2018-04-27 12:57:43 -0500

    Inline description of 'unique' behavior in jq

--- make.sh
+++ make.sh
@@ -11,6 +11,7 @@ wordlist=~/Library/Spelling/LocalDictionary
 if [[ -e $wordlist ]]; then
   prefs=Preferences.sublime-settings
   >&2 printf 'Reading wordlist at "%s" (the macOS local dictionary) and merging with "added_words" in "%s"\n' "$wordlist" "$prefs"
+  # jq's 'unique' function always returns a sorted array
   jq . "$prefs" >/dev/null && \
     jq --slurpfile words <(jq -R . "$wordlist") '.added_words = (.added_words + $words | unique)' "$prefs" | \
     sponge "$prefs"
