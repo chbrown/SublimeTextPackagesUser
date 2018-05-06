f0f7f399ac641538fd16f31996a55d0a0580e243
Christopher Brown <io@henrian.com>
2018-05-06 16:24:23 -0500

    Update set_file_exclude_patterns to read .gitignore and tsconfig.json 'exclude' field

--- TypeScript-set-file_exclude_patterns.py
+++ TypeScript-set-file_exclude_patterns.py
@@ -5,37 +5,80 @@ import sublime
 import sublime_plugin
 
 
-def typescript_output(filename):
-    basename = re.sub(r'\.tsx?$', '', filename)
-    return [basename + '.js', basename + '.d.ts']
+####################################
+# Generic Sublime Text API Helpers #
 
-
-def read_tsconfig_files(path):
+def update_window_project_data(f, window=None):
     '''
-    Open the tsconfig file and read its list of files that are not *.d.ts files
+    Get the project_data dict from the given window (defaults to the currently active window),
+    run it through f, and set the project_data on that window to the changed dict.
+    '''
+    if window is None:
+        window = sublime.active_window()
+    window.set_project_data(f(window.project_data()))
+
+
+#############################
+# TypeScript Project Config #
+
+def log(message):
+    print('[set_file_exclude_patterns]', message)
+
+
+def read_gitignore_exclude_patterns(path):
+    gitignore_path = os.path.join(path, '.gitignore')
+    try:
+        log('reading {}'.format(gitignore_path))
+        with open(gitignore_path) as fp:
+            log('excluding .gitignore patterns')
+            for line in fp:
+                gitignore_line = line.strip()
+                if not gitignore_line.startswith('#'):
+                    yield gitignore_line
+    except FileNotFoundError:
+        pass
+
+
+def read_tsconfig_exclude_patterns(path):
+    '''
+    Open the tsconfig file and read its `files` and `exclude` fields.
     '''
     tsconfig_path = os.path.join(path, 'tsconfig.json')
-    print('[set_file_exclude_patterns] reading .files from {}'.format(tsconfig_path))
-    if os.path.exists(tsconfig_path):
+    try:
+        log('reading {}'.format(tsconfig_path))
         with open(tsconfig_path) as fp:
             tsconfig = json.load(fp)
-            for file in tsconfig.get('files', []):
-                if not file.endswith('.d.ts'):
-                    yield file
+            log('excluding derivatives of .files')
+            for source in tsconfig.get('files', []):
+                basename = re.sub(r'\.tsx?$', '', source)
+                for extension in ['.js', '.d.ts']:
+                    yield basename + extension
+            log('excluding .exclude patterns')
+            for pattern in tsconfig.get('exclude', []):
+                yield pattern
+    except FileNotFoundError:
+        pass
 
 
 def update_folder(folder):
     '''
-    folder is a dict like {'path': '/Users/chbrown/github/urlio', 'file_exclude_patterns': ['*.tmp']}
+    folder is a dict like:
+        {'path': '/Users/chbrown/github/urlio',
+         'file_exclude_patterns': ['*.tmp']}
     '''
+    path = folder['path']
+    new_file_exclude_patterns = set(read_gitignore_exclude_patterns(path)) | set(read_tsconfig_exclude_patterns(path))
     original_file_exclude_patterns = folder.get('file_exclude_patterns', [])
-    typescript_sources = list(read_tsconfig_files(folder['path']))
-    typescript_outputs = {output for source in typescript_sources for output in typescript_output(source)}
-    new_typescript_outputs = list(typescript_outputs - set(original_file_exclude_patterns))
-    print('[set_file_exclude_patterns] adding {}'.format(', '.join(new_typescript_outputs)))
-    updated_file_exclude_patterns = original_file_exclude_patterns + new_typescript_outputs
+    added_file_exclude_patterns = set(new_file_exclude_patterns) - set(original_file_exclude_patterns)
+    log('adding new exclude patterns: {}'.format(', '.join(added_file_exclude_patterns)))
+    updated_file_exclude_patterns = original_file_exclude_patterns + list(added_file_exclude_patterns)
     folder.update(file_exclude_patterns=updated_file_exclude_patterns)
     return folder
+
+
+def update_project_data(project_data):
+    project_data.update(folders=list(map(update_folder, project_data.get('folders', []))))
+    return project_data
 
 
 class SetFileExcludePatternsCommand(sublime_plugin.WindowCommand):
@@ -44,13 +87,5 @@ class SetFileExcludePatternsCommand(sublime_plugin.WindowCommand):
     Use like: window.run_command("set_file_exclude_patterns")
     Check like: sublime.active_window().project_data()
     '''
-
     def run(self):
-        window = sublime.active_window()
-        project_data = window.project_data()
-        original_folders = project_data.get('folders', [])
-        updated_folders = [update_folder(folder) for folder in original_folders]
-        # why must Python make functional/immutable programming so difficult?
-        #   dict(project_data.items() + [('folders', updated_folders)]) doesn't work anymore on Python 3
-        project_data.update(folders=updated_folders)
-        window.set_project_data(project_data)
+        update_window_project_data(update_project_data)
