0b4141921623e703a7e300e099e631126b870a06
Christopher Brown <io@henrian.com>
2018-08-03 16:59:48 -0500

    Rename variables in tidying shell script

--- make.sh
+++ make.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-for json in *.sublime-*; do
-  >&2 printf 'Pretty-printing JSON file "%s" with jq\n' "$json"
+for JSON_PATH in *.sublime-*; do
+  >&2 printf 'Pretty-printing JSON file "%s" with jq\n' "$JSON_PATH"
   # Do trial run first and fail immediately,
   # to avoid truncating the file if it can't be parsed
-  jq . "$json" >/dev/null && jq . "$json" | sponge "$json"
+  jq . "$JSON_PATH" >/dev/null && jq . "$JSON_PATH" | sponge "$JSON_PATH"
 done
 
-wordlist=~/Library/Spelling/LocalDictionary
-if [[ -e $wordlist ]]; then
-  prefs=Preferences.sublime-settings
-  >&2 printf 'Reading wordlist at "%s" (the macOS local dictionary) and merging with "added_words" in "%s"\n' "$wordlist" "$prefs"
+WORDLIST_PATH=~/Library/Spelling/LocalDictionary
+if [[ -e $WORDLIST_PATH ]]; then
+  PREFS_JSON_PATH=Preferences.sublime-settings
+  >&2 printf 'Reading wordlist at "%s" (the macOS local dictionary) and merging with "added_words" in "%s"\n' $WORDLIST_PATH $PREFS_JSON_PATH
   # jq's 'unique' function always returns a sorted array
-  jq . "$prefs" >/dev/null && \
-    jq --slurpfile words <(jq -R . "$wordlist") '.added_words = (.added_words + $words | unique)' "$prefs" | \
-    sponge "$prefs"
+  jq . $PREFS_JSON_PATH >/dev/null && \
+    jq --slurpfile words <(jq -R . $WORDLIST_PATH) '.added_words = (.added_words + $words | unique)' $PREFS_JSON_PATH | \
+    sponge $PREFS_JSON_PATH
 fi
