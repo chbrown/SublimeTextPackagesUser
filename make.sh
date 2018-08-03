5faa0a5819f300c7ac112e63701198127f193d05
Christopher Brown <io@henrian.com>
2018-08-03 17:04:17 -0500

    Move/add comments

--- make.sh
+++ make.sh
@@ -13,8 +13,9 @@ WORDLIST_PATH=~/Library/Spelling/LocalDictionary
 if [[ -e $WORDLIST_PATH ]]; then
   PREFS_JSON_PATH=Preferences.sublime-settings
   >&2 printf 'Reading wordlist at "%s" (the macOS local dictionary) and merging with "added_words" in "%s"\n' $WORDLIST_PATH $PREFS_JSON_PATH
-  # jq's 'unique' function always returns a sorted array
+  # validate JSON first
   if jq . $PREFS_JSON_PATH >/dev/null; then
+    # jq's 'unique' function always returns a sorted array
     jq --slurpfile words <(jq -R . $WORDLIST_PATH) '.added_words = (.added_words + $words | unique)' $PREFS_JSON_PATH | \
       sponge $PREFS_JSON_PATH
   fi
