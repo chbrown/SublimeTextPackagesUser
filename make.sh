89b2d824b10b4a74d648fb6ecfb6cc7db97728a4
Christopher Brown <io@henrian.com>
2018-08-03 17:03:05 -0500

    Use 'if ... fi' control structures instead of &&-chaining to pre-validate JSON

--- make.sh
+++ make.sh
@@ -4,7 +4,9 @@ for JSON_PATH in *.sublime-*; do
   >&2 printf 'Pretty-printing JSON file "%s" with jq\n' "$JSON_PATH"
   # Do trial run first and fail immediately,
   # to avoid truncating the file if it can't be parsed
-  jq . "$JSON_PATH" >/dev/null && jq . "$JSON_PATH" | sponge "$JSON_PATH"
+  if jq . "$JSON_PATH" >/dev/null; then
+    jq . "$JSON_PATH" | sponge "$JSON_PATH"
+  fi
 done
 
 WORDLIST_PATH=~/Library/Spelling/LocalDictionary
@@ -12,7 +14,8 @@ if [[ -e $WORDLIST_PATH ]]; then
   PREFS_JSON_PATH=Preferences.sublime-settings
   >&2 printf 'Reading wordlist at "%s" (the macOS local dictionary) and merging with "added_words" in "%s"\n' $WORDLIST_PATH $PREFS_JSON_PATH
   # jq's 'unique' function always returns a sorted array
-  jq . $PREFS_JSON_PATH >/dev/null && \
+  if jq . $PREFS_JSON_PATH >/dev/null; then
     jq --slurpfile words <(jq -R . $WORDLIST_PATH) '.added_words = (.added_words + $words | unique)' $PREFS_JSON_PATH | \
-    sponge $PREFS_JSON_PATH
+      sponge $PREFS_JSON_PATH
+  fi
 fi
